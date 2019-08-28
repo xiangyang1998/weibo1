8fae267483b41f8c92f49675032d0c58da51c9cc
向洋 <827544120@qq.com>
2019-08-28 11:21:52 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,9 @@
 @include('layouts._header')
 <div class="container">
     <div class="offset-md-1 col-md-10">
-    @yield('content')
-    @include('layouts._footer')
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
     </div>
 </div>
 </body>
