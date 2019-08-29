533ad1fe2c595464809dba82012dc845c3cb4b7c
向洋 <827544120@qq.com>
2019-08-29 14:18:36 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,21 +20,45 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|max:50',
-            'email'=>'required|email|unique:users|max:255',
-            'password'=>'required|confirmed|min:6'
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6',
         ]);
 
         $user = User::create([
             'name' => $request->name,
-            'email'=> $request->email,
+            'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
 
         Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('user.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('user.show', $user->id);
     }
 
 }
