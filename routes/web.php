055b8c0e56d61c126bb90651a793f92136c81e15
向洋 <827544120@qq.com>
2019-08-29 18:00:29 +0800

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,13 @@ Route::delete('login','SessionsController@destroy')->name('logout');
 
 //邮件激活接口
 Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
+
+//找回密码
+//显示重置密码的邮件发送页面
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//邮箱发送重置链接
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//密码更新页面
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//执行密码更新操作
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
