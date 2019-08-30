066c7d5438e2f64f0735f80f1d3e4f4d5f6e573b
向洋 <827544120@qq.com>
2019-08-30 11:02:51 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     //主页
     public function home(){
-        return view('static_pages.home');
+        $feed_items = [];
+        if (Auth::check()){
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+        return view('static_pages.home',compact('feed_items'));
     }
     //帮助页
     public function help(){
