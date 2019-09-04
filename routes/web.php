28fb991dda32b1c27c49ce7d1cfecb0b581c246e
向洋 <827544120@qq.com>
2019-09-04 08:56:52 +0800

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 //粉丝
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+//关注用户
+Route::post('/users/followers/{user}','FollowersController@store')->name('followers.store');
+//取消用户
+Route::delete('/users/followers/{user}','FollowersController@destroy')->name('followers.destroy');
